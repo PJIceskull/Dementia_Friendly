a9d5c718e7a31a255a19cd728bfb9ae6ca9b2e8f
Pierce <Pierce@PIERCE>
2024-11-03 16:17:21 -0500

    added pages and routing function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 // Import
+import { changeRoute } from "./model.js";
 
 function initListeners() {}
 
+function initURLListener() {
+  $(window).on("hashChange", changeRoute);
+  changeRoute();
+}
+
 $(document).ready(function () {
+  initURLListener();
   initListeners();
 });
---
+++ src/model.js
@@ -0,0 +1,28 @@
+export function changeRoute() {
+  let hashTag = window.location.hash;
+  let pageID = hashTag.replace("#", "");
+  //   console.log(hashTag + ' ' + pageID);
+  // Step 1: Access the current URL
+  //   let currentURL = window.location.href;
+
+  //   // Step 2: Check if there is a hash
+  //   if (window.location.hash) {
+  //     // Step 3: Remove the hash
+  //     window.history.pushState("", document.title, currentURL.split("#")[0]);
+  //   }
+
+  // Scroll to Top of the Page
+  scroll(0, 0);
+
+  if (pageID != "") {
+    $.get(`pages/${pageID}.html`, function (data) {
+      console.log("data " + data);
+      $("#app").html(data);
+    });
+  } else {
+    $.get(`pages/home.html`, function (data) {
+      console.log("data " + data);
+      $("#app").html(data);
+    });
+  }
+}
