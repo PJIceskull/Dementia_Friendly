e1b09e13096830fee17b67563ca05e8ff35d63e3
Pierce <Pierce@PIERCE>
2024-12-19 20:42:51 -0500

    add header part

---
+++ pattern/header.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Title: Header
+ * Slug: dfIndy/header
+ * Categories: header
+ * Block Types: core/template-part/header
+ * Description: Header with site title and navigation.
+ */
+?>
+<!-- wp:group {"layout":{"type":"default"}} /-->
+<nav>
+  <a href="#home">
+    <div class="dfIndy_Logo">
+      <img
+        src="images/logos/Dementia_Friendly/DFINDY_Main_Logo.png"
+        alt="Dementia Friendly Indianapolis Logo"
+      />
+    </div>
+  </a>
+  <div class="navLinks">
+    <div class="dropDown">
+      <a href="">About Us</a>
+      <div class="navArrow">
+        <img
+          src="images/icons/keyboard_arrow_down_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.png"
+          alt="Arrow pointing Down"
+        />
+      </div>
+      <div class="dropDownMenu">
+        <div class="dropDownArrow">
+          <img
+            src="images/icons/arrow_drop_down_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.png"
+            alt="Triangle Arrow Pointing Up"
+          />
+        </div>
+        <a href="">Action Team</a>
+        <a href="">Memory Cafe</a>
+        <a href="">Living Well with Dementia Events</a>
+        <a href="">Fall Festival</a>
+      </div>
+    </div>
+    <a href="#events">Events</a>
+    <div class="dropDown">
+      <a href="#resources">Resources</a>
+      <div class="navArrow">
+        <img
+          src="images/icons/keyboard_arrow_down_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.png"
+          alt="Arrow pointing Down"
+        />
+      </div>
+      <div class="dropDownMenu">
+        <div class="dropDownArrow">
+          <img
+            src="images/icons/arrow_drop_down_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.png"
+            alt="Triangle Arrow Pointing Up"
+          />
+        </div>
+        <a href="#resources/living-with-a-diagnosis">Living With a Diagnosis</a>
+        <a href="#resources/care-partner">Care Partner</a>
+        <a href="#resources/community-member">Professional/Community Member</a>
+      </div>
+    </div>
+    <a href="#education">Education</a>
+    <a href="">Contact</a>
+    <div class="donateBTN">
+      <a href="">Donate</a>
+    </div>
+  </div>
+</nav>
+<!-- /wp:group -->
